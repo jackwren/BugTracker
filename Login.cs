741fc54ee30b35c2e2a76a238d36237bf5ee215b
jackwren <jack_wr@hotmail.co.uk>
2015-12-03 17:35:36 +0000

    Third Commit
    Made two new forms at the beginning of the program, now it loads a login
    and register form, which give s you the option of signing up or loging
    in to the home page, done through INSERT SQL statements and finding
    information in database tables

---
+++ Login.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+namespace BugReporter
+{
+    public partial class Login : Form
+    {
+       
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void sign_in_Click(object sender, EventArgs e)
+        {
+            login();
+        }
+
+        public void login()
+        {
+           
+
+            if (txt_user_name.Text != "" & txt_password.Text != "")
+            {
+                string queryText = @"SELECT Count(*) FROM users 
+                             WHERE user_name = @user_name AND password = @password";
+                using (SqlCeConnection cn = new SqlCeConnection(@"Data Source=C:\temp\Mydatabase.sdf "))
+                using (SqlCeCommand cmd = new SqlCeCommand(queryText, cn))
+                {
+                    cn.Open();
+                    cmd.Parameters.AddWithValue("@user_name", txt_user_name.Text);
+                    cmd.Parameters.AddWithValue("@password", txt_password.Text);
+                    int result = (int)cmd.ExecuteScalar();
+                    if (result > 0)
+                    {
+                        this.Hide();
+                        Home frmchild = new Home();
+                        frmchild.Show();
+                    }  
+                    else
+                    {
+                        MessageBox.Show("User Not Found!");
+                    }
+                        
+                }
+            }  
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Register reg = new Register();
+            reg.Show();
+        }
+    }
+}
---
+++ Register.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+namespace BugReporter
+{
+    public partial class Register : Form
+    {
+
+        SqlCeConnection mySqlConnection;
+
+        public Register()
+        {
+            InitializeComponent();
+            populateListBox();
+        }
+
+        public void populateListBox()
+        {
+            mySqlConnection = new SqlCeConnection(@"Data Source=C:\temp\Mydatabase.sdf ");
+
+            String selcmd = "SELECT user_name, password FROM users ORDER BY user_name";
+
+            SqlCeCommand mySqlCommand = new SqlCeCommand(selcmd, mySqlConnection);
+
+            try
+            {
+                mySqlConnection.Open();
+
+                SqlCeDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+            }
+
+            catch (SqlCeException ex)
+            {
+
+                MessageBox.Show("Failure" + ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public bool checkInputs()
+        {
+            bool rtnvalue = true;
+
+            if (string.IsNullOrEmpty(textBox1.Text) ||
+                string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Error: Please check your inputs");
+                rtnvalue = false;
+            }
+
+            return (rtnvalue);
+
+        }
+
+        public void insertRecord(String user, String pass, String commandString)
+        {
+
+            try
+            {
+                SqlCeCommand cmdInsert = new SqlCeCommand(commandString, mySqlConnection);
+
+                cmdInsert.Parameters.AddWithValue("@user", user);
+                cmdInsert.Parameters.AddWithValue("@pass", pass);
+                cmdInsert.ExecuteNonQuery();
+            }
+            catch (SqlCeException ex)
+            {
+                MessageBox.Show(user + " .." + ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkInputs())
+            {
+
+                String commandString = "INSERT INTO users(user_name, password) VALUES (@user, @pass)";
+
+                insertRecord(textBox1.Text, textBox2.Text, commandString);
+                populateListBox();
+                MessageBox.Show("Succesfully signed up, you can now log in!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
